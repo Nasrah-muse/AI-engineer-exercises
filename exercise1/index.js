c9d09276bd01e4552ba58153b5ba136d91b5fe4f
Nasrah-muse <nazronafta143@gmail.com>
2025-11-10 18:27:56 +0300

    added exercise-1 with set up user input

---
+++ exercise1/index.js
@@ -0,0 +1,13 @@
+import OpenAI from "openai"
+import dotenv from "dotenv"
+dotenv.config()
+import readline from "readline";
+
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+const input = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
