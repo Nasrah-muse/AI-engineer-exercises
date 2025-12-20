34d89f82f30ed9eb3356721dbd05fc0aab3fa1ad
Nasrah-muse <nazronafta143@gmail.com>
2025-12-20 18:49:28 +0300

    add interactive theme input for user

--- exercise2/index.js
+++ exercise2/index.js
@@ -6,3 +6,21 @@ const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN
 });
 
+import readline from "readline";
+
+function getTheme() {
+  return new Promise((resolve) => {
+    if (process.argv[2]) return resolve(process.argv[2]);
+
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    rl.question("Enter image theme: ", (answer) => {
+      rl.close();
+      if (!answer.trim()) process.exit(1)
+      resolve(answer.trim())
+    })
+  })
+}
